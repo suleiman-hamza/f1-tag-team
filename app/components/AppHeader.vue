edd4884899df341963b69b4ed7dd47f1c42ae470
suleiman hamza <hamzasuleiman530@gmail.com>
2026-03-16 16:47:54 +0100

    feat: add F1Logo component and update AppHeader; remove redundant text in index page

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -4,5 +4,10 @@
 <template>
     <header class="sticky top-0 z-50 bg-zinc-950 border-b border-zinc-800/80">
         <div class="h-0.75 bg-brand-500" />
+        <Ucontainer>
+            <div class="border">
+                <F1Logo class="text-brand-500 w-auto h-7" />
+            </div>
+        </Ucontainer>
     </header>
 </template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,6 +2,5 @@
 <template>
     <div>
         <p class="text-amber-300">Hello from the dispensation</p>
-        <p class=" border-2 bg-brand-500">Hi there</p>
     </div>
 </template>
